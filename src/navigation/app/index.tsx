bef5a52d499d07d8bcc8bc187da46c701b379006
iandev096 <iandev096@gmail.com>
2022-06-06 09:59:23 +0000

    Dark mode \ Light mode icons switch

--- src/navigation/app/index.tsx
+++ src/navigation/app/index.tsx
@@ -24,7 +24,7 @@ export default function AppStack() {
           headerRight: () => (
             <IconButton
               color={darkMode ? text : background}
-              icon={darkMode ? "moon-waning-crescent" : "white-balance-sunny"}
+              icon={darkMode ? "white-balance-sunny" : "moon-waning-crescent"}
               onPress={toggle}
             />
           ),
